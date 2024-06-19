0dde0bb3b23c76dfd296aba52569cbf7827b6ca4
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-06-19 12:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+document.getElementById('button').addEventListener('click', function (event) {
+	event.preventDefault();
+	const name = document.getElementById('name').value;
+	const year = document.getElementById('year').value;
+
+	let newUrl = 'https://localhost:8080/';
+
+	if (name || year) {
+        newUrl += '?';
+        const params = [];
+        if (name) {
+            params.push(`name=${encodeURIComponent(name)}`);
+        }
+        if (year) {
+            params.push(`year=${encodeURIComponent(year)}`);
+        }
+        newUrl += params.join('&');
+    }
+
+    // Update the h3 text
+    document.getElementById('url').innerText = newUrl;
+
+})
